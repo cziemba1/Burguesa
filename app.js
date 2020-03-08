bc697c5ff718e63232cb036078dc27f0f7a18ecc
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-03-08 08:44:14 -0300

    index page sections update

--- app.js
+++ app.js
@@ -1,83 +1,196 @@
-const hero = document.querySelector(".hero");
-const slider = document.querySelector(".slider");
-const logo = document.querySelector("#logo");
-const nav = document.querySelector("ul");
-const headline = document.querySelector(".headline");
-const headline2 = document.querySelector(".headline2");
-const colection = document.querySelector(".coleccion");
-const discover = document.querySelector(".descubrir");
+document.addEventListener("DOMContentLoaded", () => {
 
-const tl = new TimelineMax();
+    const prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
 
-tl.fromTo(hero, 1.5,
-    {
-        height: "0%"
-    },
-    {
-        height: "100%",
-        ease: Power2.easeInOut
+        var currentScrollpos = window.pageYOffset;
+
+        if (currentScrollpos > prevScrollpos) {
+            document.getElementById("nav").style.top = "-100px";
+        } else {
+            document.getElementById("nav").style.top = "0";
+        }
+
+        prevScrollpos = currentScrollpos;
+
+    }
+
+    const tl = new TimelineMax();
+
+    tl.fromTo(".hero", 1.5,
+        {
+            height: "0%"
+        },
+        {
+            height: "100%",
+            ease: Power2.easeInOut
+        })
+        .fromTo(".slider", 1.2,
+            {
+                x: "-100%"
+            },
+            {
+                x: "0%",
+                ease: Power2.easeInOut
+            }, "-=1.2")
+        .fromTo("#logo", 0.8,
+            {
+                opacity: 0,
+                x: 30
+            },
+            {
+                opacity: 1,
+                x: 0,
+                ease: Power2.easeInOut
+            }, "-=0.5")
+        .fromTo("ul", 0.8,
+            {
+                opacity: 0,
+                x: -30
+            },
+            {
+                opacity: 1,
+                x: 0,
+                ease: Power2.easeInOut
+            }, "-=1")
+        .fromTo(".headline", 0.8,
+            {
+                opacity: 0,
+                x: 60
+            },
+            {
+                opacity: 1,
+                x: 0,
+                ease: Power2.easeInOut
+            }, "-=.8")
+        .fromTo(".headline2", 0.8,
+            {
+                opacity: 0,
+                x: -60
+            },
+            {
+                opacity: 1,
+                x: 0,
+                ease: Power2.easeInOut
+            }, "-=.5")
+        .fromTo(".coleccion", 1.5,
+            {
+                opacity: 0,
+            },
+            {
+                opacity: .5,
+                ease: Power2.easeInOut
+            }, "-=.5")
+        .fromTo(".descubrir", 1.5,
+            {
+                opacity: 0,
+            },
+            {
+                opacity: 1,
+                ease: Power2.easeInOut
+            }, "-=2.2");
+
+    const controller = new ScrollMagic.Controller();
+
+    const tl2 = new TimelineMax();
+    tl2
+        .fromTo(".photo-vertical", 2, {
+            opacity: 0,
+            x: "-50%"
+        }, {
+            opacity: 1,
+            x: "0%"
+        })
+        .fromTo(".photo-horizontal", 2, {
+            opacity: 0,
+            x: "50%"
+        }, {
+            opacity: 1,
+            x: "0%"
+        }, "-=2")
+
+    let scene = new ScrollMagic.Scene({
+        triggerElement: ".photo-vertical",
+        duration: "100%",
+        triggerHook: 1,
+        offset: "100"
     })
-    .fromTo(slider, 1.2,
-        {
-            x: "-100%"
-        },
-        {
-            x: "0%",
-            ease: Power2.easeInOut
-        }, "-=1.2")
-    .fromTo(logo, 0.8,
-        {
-            opacity: 0,
-            x: 30
-        },
-        {
-            opacity: 1,
-            x: 0,
-            ease: Power2.easeInOut
-        }, "-=0.5")
-    .fromTo(nav, 0.8,
-        {
-            opacity: 0,
-            x: -30
-        },
-        {
-            opacity: 1,
-            x: 0,
-            ease: Power2.easeInOut
-        }, "-=1")
-    .fromTo(headline, 0.8,
-        {
-            opacity: 0,
-            x: 60
-        },
-        {
-            opacity: 1,
-            x: 0,
-            ease: Power2.easeInOut
-        }, "-=.8")
-    .fromTo(headline2, 0.8,
-        {
-            opacity: 0,
-            x: -60
-        },
-        {
-            opacity: 1,
-            x: 0,
-            ease: Power2.easeInOut
-        }, "-=.5")
-    .fromTo(colection, 1.5,
-        {
-            opacity: 0,
-        },
-        {
-            opacity: .5,
-            ease: Power2.easeInOut
-        }, "-=.5")
-    .fromTo(discover, 1.5,
-        {
-            opacity: 0,
-        },
-        {
-            opacity: 1,
-            ease: Power2.easeInOut
-        }, "-=2.2")
+        .setTween(tl2)
+        .addIndicators()
+        .addTo(controller);
+
+
+    const tl3 = new TimelineMax();
+    tl3
+        .fromTo(".section-2-wrapper", 1, {
+            height: 0
+        }, {
+            height: "40%"
+        })
+
+    let scene2 = new ScrollMagic.Scene({
+        triggerElement: ".section-2-wrapper",
+        triggerHook: 1,
+        offset: "-100"
+    })
+        .setTween(tl3)
+        .addIndicators()
+        .addTo(controller);
+
+
+    const tl4 = new TimelineMax();
+    tl4.fromTo(".section-3-wrapper", 1, {
+        height: 0
+    }, {
+        height: "20%"
+    })
+
+    let scene3 = new ScrollMagic.Scene({
+        triggerElement: ".section-3-wrapper",
+        triggerHook: 1,
+        offset: "-100"
+    })
+        .setTween(tl4)
+        .addIndicators()
+        .addTo(controller);
+
+
+    const tl5 = new TimelineMax();
+    tl5.fromTo("blockquote", 3, {
+        opacity: 0
+    }, {
+        opacity: 1
+    })
+
+    let scene4 = new ScrollMagic.Scene({
+        triggerElement: "blockquote",
+        triggerHook: 1,
+        offset: "-200"
+    })
+        .setTween(tl5)
+        .addIndicators()
+        .addTo(controller);
+
+    const tl6 = new TimelineMax();
+    tl6.fromTo(".insta-p", 2, {
+        y: 30
+    }, {
+        y: 0
+    })
+        .fromTo(".insta-a", 2, {
+            y: 30
+        }, {
+            y: 0
+        }, "-=2")
+
+    let scene5 = new ScrollMagic.Scene({
+        triggerElement: ".section-5",
+        triggerHook: 1,
+        offset: "100"
+    })
+        .setTween(tl6)
+        .addIndicators()
+        .addTo(controller);
+})
+
+
